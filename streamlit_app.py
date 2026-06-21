9b61ea2958fc4b3ba3f79bfca5bb30b5bbc6e0ef
Cicero Vieira <53325426+calmeidavieira@users.noreply.github.com>
2026-06-21 14:48:20 -0400

    Fix URL formatting for smoothiefroot API request

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,6 +39,6 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
 # New section to display smoothiefroot nutrition information
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
 # st.text(smoothiefroot_response.json())
 st_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = true)
