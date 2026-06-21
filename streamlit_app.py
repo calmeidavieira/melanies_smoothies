431ba6f63006f5b6f09d1c0043a34f6f32fa769c
Cicero Vieira <53325426+calmeidavieira@users.noreply.github.com>
2026-06-21 14:53:34 -0400

    Fix fruit selection and API call for nutrition info

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,9 +25,10 @@ ingredients_list = st.multiselect(
 
 if ingredients_list:
     ingredients_string = ''
-    for fruit_chose in ingredients_list:
-        ingredients_string += fruit_chose + ' '
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + 'Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         st_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
     st.write(ingredients_string)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
